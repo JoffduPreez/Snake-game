77162491a515dd37a3e0c45d168f45d629501ac4
JoffduPreez <joffre@joffredupreez.com>
2022-09-04 14:04:38 -0400

    changed refresh rate time

--- gameBoard.java
+++ gameBoard.java
@@ -3,37 +3,66 @@ import java.awt.event.*;
 import java.io.Console;
 import java.util.*;
 import javax.swing.*;
-
-
+import java.util.Timer;
 
 /*
  * This class is for the GUI, it contains all the visual components of the game
  */
 
 public class GameBoard implements KeyListener {
-    public static LinkedList<SnakeNode> snake = new LinkedList<SnakeNode>();
+    public LinkedList<SnakeNode> snake = new LinkedList<SnakeNode>();
     
-    public static JFrame frame = new JFrame();
+    public JFrame frame = new JFrame();
     
     public JPanel gridPanel = new JPanel();
-    public static JLabel[][] grid = new JLabel[20][20];
+    public JLabel[][] grid = new JLabel[20][20];
 
     public JPanel titlePanel = new JPanel();
     public JLabel titleField = new JLabel();
     public JLabel msgField = new JLabel();
 
+    public static boolean initializeGame = false;
+    public static boolean foodExists = false;
+    public static int rand1 = 0;
+    public static int rand2 = 0;
+
 
     public static void main(String[] args) {
         GameBoard board = new GameBoard();
         GameMovement controller = new GameMovement();
+        GameBoard.initializeGame = true;
+        boolean runGame = true;
 
-        controller.initializeGame(GameBoard.grid, GameBoard.snake);
-    }
+        while (true) {
+            controller.initializeGame(board.grid, board.snake);
+            board.msgField.setText("Direct snake to start game");
+            runGame = true;
+            while (runGame) {
+                if (controller.moveSnake(board.grid, board.snake, board.msgField)) { // if game is over
+                    runGame = false;
+                    board.msgField.setText("Game over");
+    
+                    // wait 2 seconds before reseting game
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        System.out.println("Sleep Exception:");
+                        e.printStackTrace();
+                    }
+    
+                    controller.resetGame(board.titleField, board.msgField, board.grid, board.snake);
+                } else {
+                    // delay loop restart by 150 ms
+                    try {
+                        Thread.sleep(125);
+                    } catch (InterruptedException e) {
+                        System.out.println("Sleep Exception:");
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
 
-    public void loadStartScreen () {
-        GameBoard.frame.setSize(600, 600);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new GridLayout(20, 20));
     }
 
     public GameBoard () {
@@ -55,7 +84,7 @@ public class GameBoard implements KeyListener {
 		msgField.setForeground(Color.white);
 		msgField.setFont(new Font("Ink Free",Font.BOLD,35));
 		msgField.setHorizontalAlignment(JLabel.CENTER);
-		msgField.setText("Direct the snake to start");
+		msgField.setText("Direct snake to start game");
 		msgField.setOpaque(true);
 
         titlePanel.setLayout(new GridLayout(2,1));
